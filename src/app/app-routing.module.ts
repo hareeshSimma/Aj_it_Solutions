29dcc20d62eb42bbde8011808dc45ab70e610348
hareeshSimma <hareesh.simma@gmail.com>
2018-10-04 00:32:16 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,16 +11,41 @@ import { UsecasesComponent } from './components/usecases/usecases.component';
 import { ServicesComponent } from './components/services/services.component';
 import { OnlineClassesComponent } from './components/online-classes/online-classes.component';
 import { OfflineClassesComponent } from './components/offline-classes/offline-classes.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { SeekerComponent } from './components/services/seeker/seeker.component';
+import { ProviderComponent } from './components/services/provider/provider.component';
+import { SupportComponent } from './components/services/support/support.component';
+import { InternshipComponent } from './components/services/internship/internship.component';
 
 const routes: Routes = [
   { path:'', component:HomeComponent },  
   { path:'ebooks', component:EBooksComponent },
   { path:'usecases', component:UsecasesComponent },
-  { path:'services', component:ServicesComponent },
-  { path:'olineclasses', component:OnlineClassesComponent },
-  { path:'offlineclasses', component:OfflineClassesComponent },
+  { path:'services', component:ServicesComponent ,
+  children:[
+    { path:'', component:SeekerComponent },
+    
+    { path:'freelanching', children:[
+      { path:'', component:SeekerComponent },
+      { path:'seeker', component:SeekerComponent },     
+      { path:'provider', component:ProviderComponent }, 
+    ]
+    
+
+},
+{ path:'support', component:SupportComponent },
+{ path:'internship', component:InternshipComponent },
+
+]
+},
+
+  { path:'olinetrainings', component:OnlineClassesComponent },
+  { path:'offlinetrainings', component:OfflineClassesComponent },
+  { path:'videos', component:VideosComponent },
+
   
-];
+
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -11,6 +10,11 @@ import { ServicesComponent } from './components/services/services.component';
 import { OnlineClassesComponent } from './components/online-classes/online-classes.component';
 import { OfflineClassesComponent } from './components/offline-classes/offline-classes.component';
 import { HomeComponent } from './components/home/home.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { SeekerComponent } from './components/services/seeker/seeker.component';
+import { ProviderComponent } from './components/services/provider/provider.component';
+import { SupportComponent } from './components/services/support/support.component';
+import { InternshipComponent } from './components/services/internship/internship.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,12 @@ import { HomeComponent } from './components/home/home.component';
     ServicesComponent,
     OnlineClassesComponent,
     OfflineClassesComponent,
-    HomeComponent
+    HomeComponent,
+    VideosComponent,
+    SeekerComponent,
+    ProviderComponent,
+    SupportComponent,
+    InternshipComponent
   ],
   imports: [
     BrowserModule,
